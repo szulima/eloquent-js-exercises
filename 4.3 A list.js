bfba316df283607d7c48388672f0c1c6657b6ef6
szulima <34941973+szulima@users.noreply.github.com>
2018-01-08 18:46:08 +0100

    Update 4.3 A list.js
    Also write a recursive version of nth.

--- 4.3 A list.js
+++ 4.3 A list.js
@@ -31,6 +31,15 @@ function nth(list,position) {
   return number;
 }
 
+function nthR(list,position) {
+  if (!list)
+    return undefined;
+  else if (position == 0)
+    return list.value;
+  else
+    return nthR(list.rest,position-1);
+}
+
 console.log(arrayToList([10, 20, 30, 40]));
 // { value: 10, rest: {value: 20, rest: {value:	30, rest:	{value: 40, rest: null }}}}
 console.log(listToArray(arrayToList([10, 20, 30])));
@@ -38,4 +47,6 @@ console.log(listToArray(arrayToList([10, 20, 30])));
 console.log(prepend(10, prepend(20, null)));
 // {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
+// 20
+console.log(nthR(arrayToList([10, 20, 30]), 1));
 // 20
