97ebc944f54c9f0ed83549ccf38739bcb5e0d97d
szulima <34941973+szulima@users.noreply.github.com>
2018-01-08 15:23:32 +0100

    Update 4.3 A list.js
    Also write the helper functions prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list.

--- 4.3 A list.js
+++ 4.3 A list.js
@@ -15,7 +15,14 @@ function listToArray(list) {
   return array;
 }
 
+function prepend(value,list) {
+  list = {value: value, rest: list};
+  return list;
+}
+
 console.log(arrayToList([10, 20, 30, 40]));
 // { value: 10, rest: {value: 20, rest: {value:	30, rest:	{value: 40, rest: null }}}}
 console.log(listToArray(arrayToList([10, 20, 30])));
 // [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// {value: 10, rest: {value: 20, rest: null}}
