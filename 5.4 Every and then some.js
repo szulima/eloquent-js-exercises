ca0c325be3abd44176b0cd8a6515ac0a0b9c292b
szulima <34941973+szulima@users.noreply.github.com>
2018-01-25 18:10:18 +0100

    Update 5.4 Every and then some.js
    Arrays also come with the standard methods every and some. Both take a predicate function that, when called with an array element as argument, returns true or false. Just like && returns a true value only when the expressions on both sides are true, every returns true only when the predicate returns true for all elements of the array. Similarly, some returns true as soon as the predicate returns true for any of the elements. They do not process more elements than necessary—for example, if some finds that the predicate holds for the first element of the array, it will not look at the values after that.
    
    Write two functions, EVERY and SOME, that behave like these methods, except that they take the array as their first argument rather than being a method.

--- 5.4 Every and then some.js
+++ 5.4 Every and then some.js
@@ -6,7 +6,18 @@ function every(array, condition) {
   return true;
 };
 
+function some(array, condition) {
+  for (var i=0; i<array.length; i++) {
+    if (condition(array[i]) == true) return true;
+  }
+  return false;
+};
+
 console.log(every([NaN, NaN, NaN], isNaN));
 // → true
 console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
 // → false
